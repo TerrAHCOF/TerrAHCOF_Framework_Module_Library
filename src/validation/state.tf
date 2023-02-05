3023061f0563e4928583e51444a2246ca2b957d5
Tosh Mackevics <toshshaw@hotmail.com>
2023-02-06 12:15:35 +1300

    Correctecd SA name

--- src/validation/state.tf
+++ src/validation/state.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terrahcof-state-ae"
-    storage_account_name = "rg-terrahcof-state-ae"
+    storage_account_name = "stterrahcofstateae"
     container_name       = "tfstate"
     key                  = "terrAHCOFvalidation.tfstate"
   }
