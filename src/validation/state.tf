1892e9fa67f14de3d18ec37ee7652a169e36b677
Tosh Mackevics <toshshaw@hotmail.com>
2023-02-06 12:12:10 +1300

    Added details and secrets for Azure SP

--- src/validation/state.tf
+++ src/validation/state.tf
@@ -1,10 +1,12 @@
 provider "azurerm" {
-  subscription_id = ""
-  tenant_id       = ""
   features {}
 }
 
 terraform {
   backend "azurerm" {
+    resource_group_name  = "rg-terrahcof-state-ae"
+    storage_account_name = "rg-terrahcof-state-ae"
+    container_name       = "tfstate"
+    key                  = "terrAHCOFvalidation.tfstate"
   }
 }
